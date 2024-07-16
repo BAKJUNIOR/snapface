4fd6127aed29d9e2ff249e15d63165cd15d320e3
BAKJUNIOR <bakbassjunior@gmail.com>
2024-07-16 17:09:54 +0000

    First commit... 2

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,13 +1,40 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FaceSnap} from "../models/face-snap";
-import {NgIf} from "@angular/common";
+import {
+  CurrencyPipe,
+  DatePipe,
+  DecimalPipe,
+  LowerCasePipe,
+  NgClass,
+  NgIf,
+  NgStyle, PercentPipe,
+  TitleCasePipe,
+  UpperCasePipe
+} from "@angular/common";
+import {FaceSnapsService} from "../services/face-snaps.service";
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
   imports: [
-    NgIf
+    // NgIf permet d'afficher ou de masquer des éléments du DOM en fonction d'une condition.
+    NgIf,
+    // DatePipe permet de formater les dates dans les templates Angular.
+    DatePipe,
+    // NgStyle permet d'appliquer des styles CSS dynamiquement aux éléments du DOM.
+    NgStyle,
+    // NgClass permet d'ajouter ou de supprimer des classes CSS dynamiquement aux éléments du DOM.
+    NgClass,
+    // LowerCasePipe permet de convertir une chaîne de caractères en minuscules.
+    LowerCasePipe,
+    // UpperCasePipe permet de convertir une chaîne de caractères en majuscules.
+    UpperCasePipe,
+    // TitleCasePipe permet de convertir une chaîne de caractères en format "Title Case" (première lettre de chaque mot en majuscule).
+    DecimalPipe,
+    PercentPipe,
+    CurrencyPipe
   ],
+
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.css'
 })
@@ -16,9 +43,12 @@ export class FaceSnapComponent implements OnInit{
   //propriété de component injectable depuis son parent ?
   @Input() faceSnap!: FaceSnap;
 
+  constructor(private faceSnapsService:FaceSnapsService) {}
+
   likeButtonText!:string;
   userHasSnapped!: boolean;
-
+  myLargeNumber:number = 4857927.18;
+  myPourcentage:number = 0.337
 
   ngOnInit():void {
 
@@ -37,13 +67,13 @@ export class FaceSnapComponent implements OnInit{
   }
 
   unlike():void {
-    this.faceSnap.addSnap();
-    this.likeButtonText = 'Oh dislike!';
+    this.faceSnapsService.snapFaceSnapById(this.faceSnap.id , "unlike");
+    this.likeButtonText = 'Oh dislike! ';
     this.userHasSnapped = false;
   }
 
   addlike():void {
-    this.faceSnap.removeSnap();
+    this.faceSnapsService.snapFaceSnapById(this.faceSnap.id , "addlike");
     this.likeButtonText = 'Oops, like!';
     this.userHasSnapped = true;
   }
--- src/app/models/face-snap.ts
+++ src/app/models/face-snap.ts
@@ -1,21 +1,39 @@
+import {SnapType} from "./snap-type.type";
+
 export class FaceSnap {
 
   location?: string;
+  id:string;
   constructor(public title: string,
               public description: string,
               public imageUrl: string,
               public createdAt: Date,
-              public like: number) {}
+              public like: number) {
+    this.id = crypto.randomUUID().substring(0, 8);
+  }
 
   addSnap(): void {
     this.like++;
   }
 
-  removeSnap(): void {
+  unlike(): void {
     this.like--;
+  }
+
+  snap(snapType: SnapType) {
+    if (snapType === 'addlike') {
+      this.addSnap();
+    } else if (snapType === 'unlike') {
+      this.unlike();
+    }
   }
 
   setLocation(location: string): void {
     this.location = location;
+  }
+
+  withLocation(location:string): FaceSnap {
+    this.setLocation(location);
+    return this;
   }
 }
